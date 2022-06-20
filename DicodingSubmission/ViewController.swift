eaccaf3ac5c696fbfc87474c2f4f89883022a13b
Rizky Faturriza <rizky.faturriza@majoo.id>
2022-06-20 19:22:34 +0700

    Initial Commit

---
+++ DicodingSubmission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicodingSubmission
+//
+//  Created by Rizky Faturriza on 20/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
